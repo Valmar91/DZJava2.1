78c2f8e1e770ba245b1fbe27cdc9b9684e00e2df
Shabrov A.L. QA-72 <valmar.chab@gmail.com>
2023-09-26 15:55:06 +0500

    ReformatCode

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-
-    int amount = 13676;
-    int bonus = 20;
-    int miles = amount/bonus;
-    System.out.println("Итоговый бонус: " + miles);
+        int amount = 13676;
+        int bonus = 20;
+        int miles = amount / bonus;
+        System.out.println("Итоговый бонус: " + miles);
     }
 }
